eaa44c08cfb501c6bb199901c8717b724fa71d1b
Gilles Coulouarn <GCoulouarn@users.noreply.github.com>
2025-09-15 22:24:43 +0200

    Ajout du sous-formulaire des sociétés pour les périmètres de consolidation
    - Nouveau modèle PerimetreConsoSociete pour la table T_J_PEC_SOC
    - Inline admin pour gérer les sociétés liées aux périmètres
    - Méthode get_societes() dans PerimetreConso
    - Colonne 'Nb Sociétés' dans la liste des périmètres
    - Autocomplete pour la sélection des sociétés
    - Libellés multilingues pour le modèle de jointure
    - Support des langues : fr, en, es, de

--- perimetre_consolidation/admin.py
+++ perimetre_consolidation/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
-from .models import PerimetreConso
+from .models import PerimetreConso, PerimetreConsoSociete
 from comptabilite.admin import AdminLabelMixin
+
+
+class PerimetreConsoSocieteInline(admin.TabularInline):
+    model = PerimetreConsoSociete
+    extra = 0
+    fields = ['societe']
+    autocomplete_fields = ['societe']
+    verbose_name = "Société"
+    verbose_name_plural = "Sociétés liées"
 
 
 @admin.register(PerimetreConso)
 class PerimetreConsoAdmin(AdminLabelMixin, admin.ModelAdmin):
-    list_display = ['id', 'code', 'libelle', 'display_name']
+    list_display = ['id', 'code', 'libelle', 'display_name', 'societes_count']
     list_filter = ['code']
     search_fields = ['code', 'libelle']
     ordering = ['code', 'libelle']
     list_display_links = ['id', 'code']
     list_editable = ['libelle']
+    inlines = [PerimetreConsoSocieteInline]
     
     def display_name(self, obj):
         """Affiche le nom complet du périmètre"""
@@ -18,12 +28,33 @@ class PerimetreConsoAdmin(AdminLabelMixin, admin.ModelAdmin):
     display_name.short_description = "Nom complet"
     display_name.admin_order_field = 'code'
     
+    def societes_count(self, obj):
+        """Affiche le nombre de sociétés liées"""
+        return obj.get_societes().count()
+    societes_count.short_description = "Nb Sociétés"
+    societes_count.admin_order_field = 'id'
+    
     fieldsets = (
         ('Informations générales', {
             'fields': ('id', 'code', 'libelle'),
             'description': 'Informations de base du périmètre de consolidation'
         }),
     )
+    
+    def changelist_view(self, request, extra_context=None):
+        """Override pour appliquer les libellés dynamiques"""
+        # Appliquer les libellés dynamiques
+        self.model.apply_dynamic_labels()
+        return super().changelist_view(request, extra_context)
+
+
+@admin.register(PerimetreConsoSociete)
+class PerimetreConsoSocieteAdmin(AdminLabelMixin, admin.ModelAdmin):
+    list_display = ['perimetre_conso', 'societe']
+    list_filter = ['perimetre_conso', 'societe']
+    search_fields = ['perimetre_conso__code', 'perimetre_conso__libelle', 'societe__nom']
+    autocomplete_fields = ['perimetre_conso', 'societe']
+    ordering = ['perimetre_conso', 'societe']
     
     def changelist_view(self, request, extra_context=None):
         """Override pour appliquer les libellés dynamiques"""
--- perimetre_consolidation/models.py
+++ perimetre_consolidation/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from comptabilite.dynamic_labels import DynamicLabelsMixin
 
 
+class PerimetreConsoSociete(DynamicLabelsMixin, models.Model):
+    """Modèle pour la table de jointure entre périmètres de consolidation et sociétés"""
+    perimetre_conso = models.ForeignKey('PerimetreConso', on_delete=models.CASCADE, db_column='PEC_Id', verbose_name="Périmètre Consolidation")
+    societe = models.ForeignKey('comptabilite.Societe', on_delete=models.CASCADE, db_column='SOC_Id', verbose_name="Société")
+
+    class Meta:
+        db_table = 'T_J_PEC_SOC'
+        verbose_name = "Périmètre Consolidation - Société"
+        verbose_name_plural = "Périmètres Consolidation - Sociétés"
+        unique_together = [['perimetre_conso', 'societe']]
+        managed = False  # Table existante, ne pas gérer par Django
+
+    def __str__(self):
+        return f"{self.perimetre_conso} - {self.societe}"
+
+
 class PerimetreConso(DynamicLabelsMixin, models.Model):
     """Modèle pour les périmètres de consolidation - équivalent à T_E_PerimetreConso_PEC"""
     id = models.IntegerField(primary_key=True, db_column='PEC_Id', verbose_name="ID")
@@ -33,3 +49,9 @@ class PerimetreConso(DynamicLabelsMixin, models.Model):
         elif self.libelle:
             return self.libelle
         return f"Périmètre {self.id}"
+
+    def get_societes(self):
+        """Retourne les sociétés liées à ce périmètre de consolidation"""
+        from comptabilite.models import Societe
+        societe_ids = PerimetreConsoSociete.objects.filter(perimetre_conso=self).values_list('societe_id', flat=True)
+        return Societe.objects.filter(id__in=societe_ids)
